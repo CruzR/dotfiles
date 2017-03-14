67d911208497d1c2fc64f1f16d392fc99cee5681
Thomas Glamsch <thomas-glamsch@gmx.de>
2017-03-15 00:13:43 +0100

    Start configuring Emacs.

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,37 @@
+; Setup package archives.
+; In addition to the standard ELPA repository, we use MELPA Stable.
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+
+; Ensure use-package is installed.
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile (require 'use-package))
+
+; Evil: Extensible vi layer for Emacs. (https://www.emacswiki.org/emacs/Evil)
+; Evil cannot currently installed from melpa-stable:
+;   https://github.com/emacs-evil/evil/issues/792
+; (use-package evil
+;   :ensure t)
+
+; Magit: A git porcelain inside Emacs. (https://magit.vc)
+(use-package magit
+  :ensure t
+  :bind ("C-x g" . magit-status))
+
+; Org mode: Your life in plain text. (http://orgmode.org)
+(use-package org
+  :ensure t)
+
+; AUCTeX: Sophisticated document creation. (https://gnu.org/software/auctex/)
+(use-package tex
+  :ensure auctex)
+
+; Prevent M-x customize from cluttering init.el
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
